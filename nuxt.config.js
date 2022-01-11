b2c2919239771057dd244cb58bb858af2ae1ff14
vibelyze <fabriziasantelia@gmail.com>
2022-01-11 16:01:40 +0100

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,9 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   ssr: false,
+  router: {
+    base: '/vibelyze/'
+  },
   generate: {
     fallback: true,
   },
